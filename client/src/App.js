fee9b41fcb08665950bcff91f604dd39cc3674d0
ConnorMinielly <Connor_Minielly@Outlook.com>
2018-07-27 01:07:59 -0400

    A Tentitively Functioning React Front End + Express Back End + Parcel Bundler App, Assembled With Much Difficulty By Myself (Surprisingly)

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './app.css';
+import axios from 'axios';
+import ReactImage from './react.png';
+
+class App extends Component {
+  state = { message: '' };
+
+  componentDidMount() {
+    axios.get('/api/message').then((res) => {
+      this.setState({ message: res.data.message });
+    });
+  }
+
+  render() {
+    const { message } = this.state;
+    return (
+      <div>
+        {message ? (
+          <h1>
+            Hello
+            {<br />}
+            {message}
+          </h1>
+        ) : (
+          <h1>
+Loading.. please wait! No.
+          </h1>
+        )}
+        <img src={ReactImage} alt="react" />
+      </div>
+    );
+  }
+}
+
+export default App;
