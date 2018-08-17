061b1a204003830844a0a815616bc032aa0b51a5
ConnorMinielly <Connor_Minielly@Outlook.com>
2018-08-17 16:50:45 -0400

    minor package change

--- server/index.js
+++ server/index.js
@@ -23,4 +23,8 @@ app.use('/gradients_api', apiRouter);
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
 app.listen(port, () => console.log('Listening on port 3000!'));
